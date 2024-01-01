f0abf8a77b352f7d9851f5c589930acc7bdbd5a5
Brenda Reimberg <brendasrsouza@gmail.com>
2024-01-01 16:13:20 -0300

    Criação do arquivo pokemon-model.js e alteração nos arquivos poke-api.js e main.js, adição do script pokemon-model no arquivo index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,18 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-    <span class="number">#${pokemon.order}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-        <ol class="types">
-            ${convertPokemonTypesToLi(pokemon.types).join('')}
-        </ol>
-        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-    </div>
-</li>
-`
+        <li class="pokemon">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class='type'>${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" 
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
 }
 
 const pokemonList = document.getElementById('pokemonList');
@@ -23,5 +21,6 @@ const pokemonList = document.getElementById('pokemonList');
 pokemons em uma lista de "li"; juntar essas listas de li sem separador.
 Irá criar um html que irá concatenar com o html existente.*/
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml= pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,31 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     //um novo fetch para url do pokemon que quer acessar e convertendo, o repsonse que retornar, em JSON; Continua em detailRequests próximo ao final do código;
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?ofset=${offset}&limit=${limit}`
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
         //converter o response em json
