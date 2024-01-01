c6a3b6b54927d9661a4e2a54f1a3edbbc8fe8866
Brenda Reimberg <brendasrsouza@gmail.com>
2024-01-01 11:24:26 -0300

    alterações nos arquivos javascript main e poke-api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-    <span class="number">#0001</span>
+    <span class="number">#${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-            <li class="type">Grass</li>
-            <li class="type">Poison</li>
+            ${convertPokemonTypesToLi(pokemon.types).join('')}
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
     </div>
 </li>
 `
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,21 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) =>  {
-const url = `https://pokeapi.co/api/v2/pokemon?ofset=${offset}&limit=${limit}`
-return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url)))
+pokeApi.getPokemonDetail = (pokemon) => {
+    //um novo fetch para url do pokemon que quer acessar e convertendo, o repsonse que retornar, em JSON; Continua em detailRequests próximo ao final do código;
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?ofset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        //converter o response em json
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        //mapear a lista de pokémons em uma lista de requisições dos detalhes do pokémons e chama o pokeApi.getPokemonDetail, no iníco do código;
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        //lista de promessa a ser resolvida;
+        .then((detailRequests) => Promise.all(detailRequests))
+        //quando a lista for resolvida apresenta a lista de detalhes dos pokemons
+        .then((pokemonsDetails) => pokemonsDetails)
 }
