f1f8741cb0bea4adbf7be4fb2d38454562f91373
Brenda Reimberg <brendasrsouza@gmail.com>
2024-01-02 18:20:29 -0300

    Modificações nos arquivos pokedex.css, style.css, main.js, poke-api.js e index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,57 @@
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 8;
+let offset = 0;
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}" 
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+function loadPokemonItems(offset, limit) {
+    /*Pegar a lista de pokemons, mapear os pokemons, converter essa lista de 
+    pokemons em uma lista de "li"; juntar essas listas de li sem separador.
+    Irá criar um html que irá concatenar com o html existente.*/
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" 
+                         alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItems(offset, limit)
 
-/*Pegar a lista de pokemons, mapear os pokemons, converter essa lista de 
-pokemons em uma lista de "li"; juntar essas listas de li sem separador.
-Irá criar um html que irá concatenar com o html existente.*/
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml= pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItems(offset, limit)
 })
+/*
+=======================================================================
+LIMITAR PARA PRIMEIRA GERAÇÃO DE POKÉMONS (regra de paginação):
+
+const maxRecords = 151
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordNexPage = offset + limit
+
+    if(qtdRecordNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItems(offset, limit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    } else {
+        loadPokemonItems(offset, newLimit)
+    }
+})
+=======================================================================
+*/
